de36e728faba02b118862ff7d91369ce853cf37a
Rafee Jenkins <rafeejenkins@gmail.com>
2022-09-11 16:35:51 +0100

    hotfix email

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -7,7 +7,7 @@ export default function SiteFooter() {
         <footer className="justify-items-center bg-offwhite h-10 flex justify-center pb-2 md:pb-0">
             <h1 className="font-merri text-center m-3">{new Date().getFullYear()} Relli</h1>
             <h1 className="font-merri text-center m-3">-</h1>
-            <h1 className="font-merri text-center m-3 underline" onClick={() => { window.location.href = 'mailto:rafee@gethala.app'; logEvent(analytics, 'email_me') }}>Get in Touch</h1>
+            <h1 className="font-merri text-center m-3 underline" onClick={() => { window.location.href = 'mailto:rafee@gethalal.app'; logEvent(analytics, 'email_me') }}>Get in Touch</h1>
         </footer>
     )
 }
